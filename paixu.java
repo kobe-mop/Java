4d81211c60624c6badc50430195f4792b87de569
asb <kobe-mop@qq.com>
2016-11-17 19:44:12 +0800

    paixu commit

---
+++ paixu.java
@@ -0,0 +1,11 @@
+/*
+第一步
+for (int x=0;x<arr.length ;x++ ) {
+     for (int y=0;y<arr.length ;y++ ) {
+第二步
+for (int x=0;x<arr.length-1 ;x++ ) {
+     for (int y=0;y<arr.length ;y++ ) {
+第三步
+for (int x=0;x<arr.length-1 ;x++ ) {
+     for (int y=0;y<arr.length-x-1 ;y++ ) {
+*/
